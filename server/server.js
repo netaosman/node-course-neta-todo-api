114ae312ca6bae88f055c3d89836fb4909a035c4
neta <netaosman@gmail.com>
2017-10-01 13:37:39 +0300

    add post and todos

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ var mongoose = require('./db/mongoose.js');
 var { Todo } = require('./models/todo.js');
 var { User } = require('./models/user.js');
 
+
+
 var app = express();
 
 app.use(bodyParser.json());
@@ -22,6 +24,17 @@ app.post('/todos', (req, res) => {
     });
 });
 
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (err) => {
+        res.status(400).send(e);
+    });
+});
+
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
+
+module.exports = {app};
